1ee5c939c8e158e54b5664e4259a8d0e1a7763db
kongqing <810131571@qq.com>
2017-05-27 13:51:41 +0800

    //异地登录强制退出

--- app/src/main/java/cn/com/shequnew/pages/activity/ContentFileDetailsActivity.java
+++ app/src/main/java/cn/com/shequnew/pages/activity/ContentFileDetailsActivity.java
@@ -395,7 +395,7 @@ public class ContentFileDetailsActivity extends BaseActivity implements CommentA
     }
 
     private void sendMessage() {
-        if (String.valueOf(AppContext.cv.getAsInteger("id")).trim().isEmpty()) {
+        if (String.valueOf(AppContext.cv.getAsInteger("mobile")).trim().isEmpty()) {
             return;
         }
         Intent intent = new Intent(ContentFileDetailsActivity.this, ChatActivity.class);
