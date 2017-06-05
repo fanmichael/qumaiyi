c0d5eff958eb92c68d13593da16d423eaf420f39
jingkun.chen <791068331@qq.com>
2017-06-05 16:11:13 +0800

    提交微信支付修改

--- app/src/main/java/com/yshstudio/originalproduct/wxapi/WXPayEntryActivity.java
+++ app/src/main/java/com/yshstudio/originalproduct/wxapi/WXPayEntryActivity.java
@@ -1,18 +1,26 @@
 package com.yshstudio.originalproduct.wxapi;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.tencent.mm.opensdk.modelbase.BaseReq;
 import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
 import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
 
 import java.io.IOException;
 
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 import com.yshstudio.originalproduct.inc.Ini;
+import com.yshstudio.originalproduct.tools.Constants;
 import com.yshstudio.originalproduct.tools.SharedPreferenceUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
@@ -30,13 +38,27 @@ import okhttp3.Response;
 
 
 public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
+    private IWXAPI api;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        api = WXAPIFactory.createWXAPI(this, Constants.APP_ID, false);
+        try {
+            api.handleIntent(getIntent(), this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         Log.e("aa", "sssssssssssssssssssssssssssssssssssssssssssssssssssssss");
-        chaxunjieguo();
+//        chaxunjieguo();
     }
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+//        setIntent(intent);
+//        api.handleIntent(intent, this);
+    }
     private void chaxunjieguo() {
         OkHttpClient client = new OkHttpClient();
         RequestBody requestBody = new FormBody.Builder()
@@ -55,6 +77,11 @@ public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
             public void onResponse(Call call, Response response) throws IOException {
                 String result = response.body().string();
                 //根据这个result结果做你们需求的操作，result实际就是支付的结果json数据
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 finish();
             }
         });
@@ -62,12 +89,21 @@ public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
 
     @Override
     public void onReq(BaseReq baseReq) {
+        Log.e("aa", "sssssssssssssssssssssssssssssssssssssssssssssssssssssss");
+
         chaxunjieguo();
     }
 
     @Override
     public void onResp(BaseResp baseResp) {
-
+        finish();
+        if (baseResp.errCode==0){
+            Toast.makeText(getApplicationContext(),"支付成功",Toast.LENGTH_LONG).show();
+        }else if(baseResp.errCode==-1){
+            Toast.makeText(getApplicationContext(),"支付失败",Toast.LENGTH_LONG).show();
+        }else if(baseResp.errCode==-2){
+            Toast.makeText(getApplicationContext(),"取消支付",Toast.LENGTH_LONG).show();
+        }
 
     }
 }
